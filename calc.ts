f43615b13cce46315ef4a4bbb52fabab4ca4500f
Albrecht Uhlig <albrecht.uhlig@gmail.com>
2021-05-20 09:05:35 +0200

    only handle get

--- calc.ts
+++ calc.ts
@@ -1,7 +1,27 @@
-addEventListener('fetch', (event) => {
-    const answer = 42
-    const response = new Response(`The answer is: ${answer}`, {
-        headers: { 'content-type': 'text/plain' }
+function welcomeMessage() {
+    return new Response("Hello, welcome!!!!", {
+        headers: {
+            'content-type': 'text/plain',
+            'test-header':  '123'
+        }
     })
-    event.respondWith(response)
+}
+
+function errorResponse() {
+    return new Response("Error, kein plan.", {
+        headers: {
+            'content-type': 'text/plain',
+        },
+        status: 405
+    })
+}
+
+function handleRequest( request: any ) {
+    if( request?.method === "GET" ) {
+        return welcomeMessage()
+    }
+    return errorResponse()
+}
+addEventListener('fetch', (event) => {
+    event.respondWith(handleRequest(event.request))
 })
