088698d9e186ee744f7327e58cef7e029759a01c
Albrecht Uhlig <albrecht.uhlig@gmail.com>
2021-05-20 09:46:03 +0200

    add calc stuff

--- calc.ts
+++ calc.ts
@@ -17,10 +17,42 @@ function errorResponse() {
     })
 }
 
-function handleRequest( request: any ) {
+function calculate( par1: number, par2: number, op: string ): number | string {
+    if( op === "add" ) {
+        return par1 + par2
+    }
+    if( op === "sub" ) {
+        return par1 - par2
+    }
+    if( op === "mul" ) {
+        return par1 * par2
+    }
+    if( op === "div" ) {
+        return par1 / par2
+    }
+    return "Error"
+}
+
+async function handleRequest( request: any ) {
     if( request?.method === "GET" ) {
         const ip = request.headers.get("x-forwarded-for");
         return welcomeMessage(ip)
+    }
+    if( request?.method === "POST" ) {
+        const requestData = await request.json()
+        const { par1, par2, op } = requestData
+        const returnedValue = calculate( par1, par2, op )
+        let statusCode = 405
+        if( typeof returnedValue === 'number' ) {
+            statusCode = 200
+        }
+        return new Response( `${returnedValue}`, {
+            headers: {
+                'content-type': 'text/plain',
+                'test-header':  '123'
+            },
+            status: statusCode
+        })
     }
     return errorResponse()
 }
